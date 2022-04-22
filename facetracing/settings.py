3623d2b02fc4fbd1d94e0217dec08d1974508c27
Xiaoting Wang <xiaotinw@andrew.cmu.edu>
2022-04-22 19:14:24 -0400

    Removed the secret key

--- facetracing/settings.py
+++ facetracing/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b+#s7oy9dzdaypu9dj#s1hst_y72kw$b80zhp9m)s6l^xra=+^'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
