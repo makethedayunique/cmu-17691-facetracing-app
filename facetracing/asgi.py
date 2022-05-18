baedb9d0a3bb138840efdd5ab98f36772fec59a0
Xiaoting Wang <xiaotinw@andrew.cmu.edu>
2022-05-17 22:52:37 -0400

    Automate configuration with django-heroku

--- facetracing/asgi.py
+++ facetracing/asgi.py
@@ -8,15 +8,9 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-import django
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facetracing.settings')
-django.setup()
 
-import errands.routing
-from channels.routing import get_default_application
-
-# application = get_asgi_application()
-application = get_default_application()
+application = get_asgi_application()
