c23640c4d52ba23220817c23e98677344dd51802
Xiaoting Wang <xiaotinw@andrew.cmu.edu>
2022-05-17 23:00:29 -0400

    Modified the settings file

--- facetracing/settings.py
+++ facetracing/settings.py
@@ -15,7 +15,6 @@ import environ
 import os
 import django_heroku
 
-django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -136,3 +135,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'face/static/face/images/temp/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
+
